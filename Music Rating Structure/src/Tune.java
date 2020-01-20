539130114bad75ebfe223d52969a48640ebcf4c8
ian-hank <57643516+ian-hank@users.noreply.github.com>
2020-01-20 14:09:40 -0500

    Setting up classes
    Setting up a Tune. A tune is the overall object for a song. Including all the details of it including the rating. Tune will be the overlaying Object type for the structure.  Tunes will have objects built in to complete an entire "Tune".

---
+++ Music Rating Structure/src/Tune.java
@@ -0,0 +1,78 @@
+
+public class Tune {
+	
+	private static Song song;				//Name of the Song
+	private static Album album;				//Name of the Album
+	private static Author author;			//Name of the Author
+	private static Genre genre;				//Name of the Genre
+	private static Length length;			//Length of the Song
+	private static Rating rating;			//Overall rating of the Song
+	
+	public Tune() {
+	}
+	
+	public Tune(Song song, Album album, Author author, Genre genre, Length length, Rating rating) {
+		
+	}
+	
+	public String getSong() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAlbum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getAuthor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getGenre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getFinalRating() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String setSong() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String setAlbum() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String setAuthor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String setGenre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int setLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double setFinalRating() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

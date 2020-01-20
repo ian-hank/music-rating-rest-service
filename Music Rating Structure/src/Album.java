b837014a1fdea1cd8f4d4d5a537c9be82a76f9df
ian-hank <57643516+ian-hank@users.noreply.github.com>
2020-01-20 14:44:41 -0500

    Finishing the Song and Album objects
    Creating the starting Song and Album objects that are contained within a Tune. First design of these with user input and text entry of creating a new one. Created with setters and getters.

--- Music Rating Structure/src/Album.java
+++ Music Rating Structure/src/Album.java
@@ -1,4 +1,30 @@
+//Needed Imports
+import java.util.Scanner;
 
 public class Album {
-
+	
+	private static String name;		//Name of the Album
+	
+	//Creating new Album through user input
+	public Album() {
+		@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
+		System.out.println("What is the name of the song?: ");
+		name = input.nextLine();
+	}
+	//Creating new Album through text
+	public Album(String name) {
+		Album.name = name;
+	}
+	
+	//Getting the name of the Album
+	public String getName() {
+		return name.toString();
+	}
+	
+	//Setting the name of the Album
+	public boolean setName(String name) {
+		Album.name = name;
+		return Album.name == name;
+	}
 }

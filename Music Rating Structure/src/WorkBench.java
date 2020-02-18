04306a69e010ed61e6eab2f4b9ea9a10156ced6c
ian-hank <57643516+ian-hank@users.noreply.github.com>
2020-02-17 21:03:55 -0500

    Finising OOS(ObjectOutputStream) and OIS
    Begun to be able to read and write objects, still need to set up and finish the main loop where the structure can be manipulated

--- Music Rating Structure/src/WorkBench.java
+++ Music Rating Structure/src/WorkBench.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -11,8 +10,12 @@ public class WorkBench {
 	public static void main(String[] args) throws IOException, InterruptedException {
 		Scanner input = new Scanner(System.in);
 		
+		//Constant Variables
+		final String FILE_PATH = "myDatabase.bin";
+		
 		//Instance Variables
-		String enter;
+		String menuChoice, currentChoice;
+		TuneArray mainArray = new TuneArray();
 		
 		//Main Menu
 		printMainMenu();
@@ -20,34 +23,39 @@ public class WorkBench {
 		//Input validation for entering the Music Rating Database
 		do {
 		    System.out.print("What would you like to do[1-4]?: ");
-		    enter = input.nextLine().toLowerCase();
+		    menuChoice = input.nextLine().toLowerCase();
 		}
-		while (!"1".equals(enter) && !"2".equals(enter) && !"3".equals(enter) && !"4".equals(enter));
-		
-		//Array for Songs
-		TuneArray mainArray = new TuneArray();
+		while (!"1".equals(menuChoice) && !"2".equals(menuChoice) && !"3".equals(menuChoice) && !"4".equals(menuChoice));
 		
 		//New Setup
-		if (enter.equals("1")) {
-			introduction();
+		if (menuChoice.equals("1")) {
+			System.out.println("\nWelcome to your personal music rating database!");
+			System.out.println("Well have you start by rating your first song!");
 			Tune newTune = userInputTune(input);
+			mainArray.add(newTune);
+			mainArray.add(newTune);
+			mainArray.add(newTune);
 			mainArray.add(newTune);
 			writeToFile(mainArray);
 			
-			System.out.println("Congrats! Your first song is in the database under the file myDatabase.bin!");
+			System.out.println("\nCongrats! Your first song is in the database under the file myDatabase.bin!\nWe will now enter the main screen for manipulating your tunes!");
 			
 		//Quick Start
-		} else if (enter.equals("2")) {
-			System.out.print("Please enter file path to myDatabase.bin: ");
-			String filepath = input.next();
-			mainArray = importFromFile(filepath);
+		} else if (menuChoice.equals("2")) {
+			System.out.println("Importing all Tunes to array!");
+			TuneArray tempArray = importFromFile(FILE_PATH);
+			
+			for (int i = 0; i < tempArray.size(); i++) {
+				mainArray.set(i, tempArray.get(i));
+			}
 			
 		//About
-		} else if (enter.equals("3")) {
+		} else if (menuChoice.equals("3")) {
 			//TODO Finish The About
+			System.out.println("Yea we will finish it eventually!");
 			
 		//Exit
-		} else if (enter.equals("4")) {
+		} else if (menuChoice.equals("4")) {
 			System.out.print("Well, I hope to see you soon! Now Exiting");
 			for (int i = 0; i <= 2; i++) {
 				Thread.sleep(950);
@@ -56,41 +64,63 @@ public class WorkBench {
 			System.exit(0);
 		}
 		
-		String filepath = "myDatabase.bin";
+		do {
+			
+			System.out.println("\n ________________________");
+			System.out.println("|  Music Rating Database |");
+			System.out.println("|                        |");
+			System.out.println("|      --Options--       |");
+			System.out.println("|1: Add New Tune         |");
+			System.out.println("|2: Remove Tune          |");
+			System.out.println("|3: Get Tune(Index)      |");
+			System.out.println("|4: Exit --> Save/Write  |");
+			System.out.println("|						 |");
+			System.out.println("|                        |");
+			System.out.println("|                        |");
+			System.out.println("|                        |");
+			System.out.println("|________________________|");
+			System.out.print("==========================> ");
+			currentChoice = input.nextLine();
+			
+			if (currentChoice.equals("1")) {
+				Tune newTune = userInputTune(input);
+				mainArray.add(newTune);
+				continue;
+			} else if (currentChoice.equals("2")) {
+				
+			} else if (currentChoice.equals("3")) {
+				
+			} else if (currentChoice.equals("4")) {
+				
+			} else {
+				System.out.println("Im sorry I couldnt understand your response! Please enter a number!");
+				continue;
+			}
+			
+		} while (!currentChoice.equals("4"));
 		
-		TuneArray tempArray = new TuneArray();
-		tempArray = importFromFile(filepath);
 		
-		for (int i = 0; i < tempArray.size(); i++) {
-			mainArray.add(tempArray.get(i));
+		
+	}
+	
+	//Prints the Main Menu
+		public static void printMainMenu() {
+			System.out.println(" ________________________");
+			System.out.println("| Music Rating Database  |");
+			System.out.println("|                        |");
+			System.out.println("|      Version 1.0       |");
+			System.out.println("|                        |");
+			System.out.println("|                        |");
+			System.out.println("|      By: Ian Hank      |");
+			System.out.println("|                        |");
+			System.out.println("|     1: New Setup       |");
+			System.out.println("|     2: Quick Start     |");
+			System.out.println("|     3: About           |");
+			System.out.println("|     4: Exit            |");
+			System.out.println("|________________________|");
 		}
-		System.out.println("Can we get to here");
-		mainArray.printQuickTunes();
 		
-	}
-	
-	public static void introduction() {
-		System.out.println("\nWelcome to your personal music rating database!");
-		System.out.println("Well have you start by rating your first song!");
-
-	}
-	
-	public static void printMainMenu() {
-		System.out.println(" ________________________");
-		System.out.println("| Music Rating Database  |");
-		System.out.println("|                        |");
-		System.out.println("|      Version 1.0       |");
-		System.out.println("|                        |");
-		System.out.println("|                        |");
-		System.out.println("|      By: Ian Hank      |");
-		System.out.println("|                        |");
-		System.out.println("|     1: New Setup       |");
-		System.out.println("|     2: Quick Start     |");
-		System.out.println("|     3: About           |");
-		System.out.println("|     4: Exit            |");
-		System.out.println("|________________________|");
-	}
-	
+	//Gets user input to create a new Tune
 	public static Tune userInputTune(Scanner input) {
 		String song, album, author, genre;
 		Rating rating;
@@ -119,44 +149,48 @@ public class WorkBench {
 		return newSong;
 	}
 	
+	//Writes Tunes to a binary file
 	public static void writeToFile(TuneArray array) throws IOException {
 		FileOutputStream out = new FileOutputStream("myDatabase.bin");
-		
 		ObjectOutputStream objOut = new ObjectOutputStream(out);
-		
-		objOut.write(array.size());
 		
 		for (int i = 0; i < array.size(); i++) {
 			objOut.writeObject(array.get(i));	
 		}
+		Tune endTune = new Tune();
+		endTune.setLength(-1);
+		objOut.writeObject(endTune);
 		
 		objOut.close();
 	}
 	
-	public static TuneArray importFromFile(String filepath) {
+	//Imports Tunes from binary file
+	public static TuneArray importFromFile(String filepath) throws FileNotFoundException {
 		TuneArray tempArray = new TuneArray();
-		int count;
+		int count = 0;
+		
+		FileInputStream fileIn = new FileInputStream(filepath);
 		try {
-			 
-			FileInputStream fileIn = new FileInputStream(filepath);
+			@SuppressWarnings("resource")
 			ObjectInputStream objectIn = new ObjectInputStream(fileIn);
 			
-			count = objectIn.readInt();
-			
-			for (int i = 0; i < count; i++) {
-				System.out.println("Trial");
+			boolean cont = true;
+			while (cont) {
 				Tune obj = (Tune) objectIn.readObject();
-				tempArray.add(obj);
-				System.out.println("Object " + (i+1) + ": " + obj.tuneToString());
+				if (obj.getLength() != -1) {
+					tempArray.add(obj);
+					count++;
+					System.out.println("Object " + count + ": " + tempArray.get(count - 1) );
+				} else if (obj.getLength() == -1) {
+					cont = false;
+					return tempArray;
+				} 
 			}
-           
-           
-           objectIn.close();
- 
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-		
+			objectIn.close();
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return tempArray;
 	}
 }

70794cd15bfbd069c71e55283ac6d62b77b435a5
ian-hank <57643516+ian-hank@users.noreply.github.com>
2020-12-30 15:17:15 -0500

    Refactoring into a spring boot + mongoDB api
    Pretty close to a fresh start. Will be refactoring and copying some code from the previous branch. This will turn the app into an api/rest service to then be turned into a web application.

---
+++ spring-mongo-api/src/main/java/com/ichprograms/rating/music/springmongoapi/SpringMongoApiApplication.java
@@ -0,0 +1,13 @@
+package com.ichprograms.rating.music.springmongoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoApiApplication.class, args);
+	}
+
+}
---
+++ spring-mongo-api/src/test/java/com/ichprograms/rating/music/springmongoapi/SpringMongoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ichprograms.rating.music.springmongoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

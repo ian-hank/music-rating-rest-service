0d9dc420218f8c7d406d974526d0a499c2d39ca6
ian-hank <57643516+ian-hank@users.noreply.github.com>
2020-12-30 18:55:15 -0500

    First Stable Spring Mongo Music API
    Turning Tune object into a resource to be used by a rest controller. Hosted on a local host mongoDB server.

--- spring-mongo-api/src/main/java/com/ichprograms/rating/music/springmongoapi/SpringMongoApiApplication.java
+++ spring-mongo-api/src/main/java/com/ichprograms/rating/music/springmongoapi/SpringMongoApiApplication.java
@@ -2,8 +2,12 @@ package com.ichprograms.rating.music.springmongoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.ichprograms.rating.music")
+@EnableMongoRepositories
 public class SpringMongoApiApplication {
 
 	public static void main(String[] args) {

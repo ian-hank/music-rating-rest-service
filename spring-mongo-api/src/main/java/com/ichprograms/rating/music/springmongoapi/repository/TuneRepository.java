0649769b3d9199d9de7c5bec9be5f6c21281f808
ian-hank <57643516+ian-hank@users.noreply.github.com>
2021-01-13 11:50:33 -0500

    Ateemtping to a do a fresh maven build

--- spring-mongo-api/src/main/java/com/ichprograms/rating/music/springmongoapi/repository/TuneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ichprograms.rating.music.springmongoapi.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ichprograms.rating.music.springmongoapi.model.Tune;
-
-public interface TuneRepository extends MongoRepository<Tune, Integer>{
-
-}

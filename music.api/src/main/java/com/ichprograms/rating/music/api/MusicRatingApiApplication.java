57c62c7a464d3fcf5a88c535ef86d7655bee84cb
ian-hank <57643516+ian-hank@users.noreply.github.com>
2021-01-13 11:59:26 -0500

    Spring Intializer build added
    Using Spring Data MongoDB, Spring Web, Spring Security, DevTools

---
+++ music.api/src/main/java/com/ichprograms/rating/music/api/MusicRatingApiApplication.java
@@ -0,0 +1,13 @@
+package com.ichprograms.rating.music.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRatingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicRatingApiApplication.class, args);
+	}
+
+}
